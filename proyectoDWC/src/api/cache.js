1449086629bf244992bb23cfc1c43d5a0a22cd94
aaron <aaronetico@gmail.com>
2026-05-08 14:34:35 +0200

    Updates

--- proyectoDWC/src/api/cache.js
+++
@@ -1,8 +0,0 @@
-export const cache = {
-  brands: null,
-  modelsByBrand: new Map(),
-  yearsByModel: new Map(),
-  versionsByYear: new Map(),
-  productsByVersion: new Map(),
-  productById: new Map()
-}
